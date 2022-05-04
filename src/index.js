d5c68b020acd71ea624e6e1b0838e9a425ecc1e4
ALiasota <90059332+ALiasota@users.noreply.github.com>
2022-05-04 19:50:24 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+import HeaderPage1 from './HeaderPage1';
+import HeaderLib from './HeaderLib.js';
+
+// HeaderLib();
+
+const root = document.getElementById('root');
+root.innerHTML = HeaderPage1();
+// root.innerHTML = HeaderLib();
