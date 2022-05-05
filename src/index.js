0aeb030f1744f2898cdbda757a15cf8c622b40d9
ALiasota <90059332+ALiasota@users.noreply.github.com>
2022-05-05 12:33:44 +0300

    logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,59 @@
 import './sass/main.scss';
-import HeaderPage1 from './HeaderPage1';
-import HeaderLib from './HeaderLib.js';
+import HeaderPage1 from './js/headerPage1/HeaderPage1';
+import HeaderLib from './js/HeaderLib/HeaderLib';
 
-// HeaderLib();
+const header = document.getElementById('header');
+const content = document.getElementById('content');
+onClickHome();
+// header.innerHTML = HeaderPage1();
+// getLogo();
+// getLibr();
 
-const root = document.getElementById('root');
-root.innerHTML = HeaderPage1();
-// root.innerHTML = HeaderLib();
+function onClickHome() {
+  header.innerHTML = HeaderPage1();
+  getLogo();
+  getLibr();
+  content.innerHTML = '<h1>Home</h1>';
+}
+function onClickLibrary() {
+  header.innerHTML = HeaderLib();
+  content.innerHTML = '<h1>Library</h1>';
+  getLogo();
+  getButtons();
+  getHome();
+}
+
+function getLibr() {
+  const libr = document.querySelector('.library-link');
+  libr.addEventListener('click', onClickLibrary);
+  libr.classList.remove('item-current');
+}
+
+function getHome() {
+  const home = document.querySelector('.home-link');
+  home.addEventListener('click', onClickHome);
+  home.classList.remove('item-current');
+}
+
+function getButtons() {
+  const watchet = document.querySelector('.header__btn--watchet');
+  const queue = document.querySelector('.header__btn--queue');
+  watchet.addEventListener('click', () => {
+    watchet.classList.add('btn-active');
+    queue.classList.remove('btn-active');
+    content.innerHTML = '<h1>watchet</h1>';
+  });
+
+  queue.addEventListener('click', () => {
+    queue.classList.add('btn-active');
+    watchet.classList.remove('btn-active');
+    content.innerHTML = '<h1>queue</h1>';
+  });
+}
+
+function onClickWatched() {}
+
+function getLogo() {
+  const logo = document.querySelector('.logo');
+  logo.addEventListener('click', onClickHome);
+}
--- src/js/headerPage1/HeaderPage1.js
+++ src/js/headerPage1/HeaderPage1.js
@@ -1,6 +1,6 @@
-export default function HeaderLib() {
-  return `<header class="header-lib">
-    <div class="header-wrapper header-wrapper--libr">
+export default function HeaderPage1() {
+  return `<section class="header">
+    <div class="header-wrapper">
         <div class="header-top">
             <div class="logo">
                 <a href='#' class="logo-link">
@@ -16,25 +16,27 @@ export default function HeaderLib() {
             <nav class="nav-menu">
                 <ul class="list-nav">
                     <li class="item ">
-                        <a href="#" class="home-link">
+                        <a href="#" class="item-current home-link">
                             Home
                         </a>
                     </li>
                     <li class="item">
-                        <a href="#" class="item-current library-link">
-                            My library
+                        <a href="#" class="library-link">
+                        My library
                         </a>
                     </li>
                 </ul>
             </nav>
-            
         </div>
-        <div class="header__btn-cont">
-            <button type="button" class="header__btn header__btn--watchet">WATCHED</button>
-            <button type="button" class="header__btn header__btn--queue">QUEUE</button>
-        </div>
+        <form class="header-form">
+            <input class='header-input' type="text" name="filmName" placeholder="Поиск фильмов" />
+            <button class='header-button--submit' type="submit">
+                <svg width="12px" height="12px" class="input-images">
+                    <use href="./images/headerPage1/symbol-defs.svg#search"></use>
+                </svg>
+            </button>
+        </form>
     </div>
-    
-</header>
+</section>
 `;
 }
