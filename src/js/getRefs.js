a287ff50faec422cd15fe17eba2fa0cb4727f668
Maksym Kravchenko <maks.cross@gmail.com>
2022-05-05 10:06:24 +0300

    add js folders structure

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,7 +1,11 @@
 export function getRefs() {
   return {
-    // buttonMyLibrary: document.querySelector('.'),
-    // buttonWatched: document.querySelector('.'),
-    // buttonQueue: document.querySelector('.'),
+    // buttonMyLibrary: document.querySelector(''),
+    // buttonWatched: document.querySelector(''),
+    // buttonQueue: document.querySelector(''),
   };
 }
+
+//   import if files
+// import { getRefs } from '../getRefs';
+// const { link1, link2, link3 } = getRefs();
