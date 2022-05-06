2b77f76bf60cf14d9f48350da235d4d86d7ea01f
Kostya Get <progogyd@gmail.com>
2022-05-06 09:20:52 +0300

    setings button

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -1,11 +1,16 @@
-export function getRefs() {
-  return {
-    // buttonMyLibrary: document.querySelector(''),
-    // buttonWatched: document.querySelector(''),
-    // buttonQueue: document.querySelector(''),
-  };
-}
+// export function getRefs() {
+//   return {
+//     buttonMyLibrary: document.querySelector(''),
+//     buttonWatched: document.querySelector(''),
+//     buttonQueue: document.querySelector(''),
+//   };
+// }
 
 //   import in files
 // import { getRefs } from '../getRefs';
 // const { link1, link2, link3 } = getRefs();
+const refs = {
+  libraryButtons: document.querySelector('.header__btn-cont'),
+  buttonWatched: document.querySelector('.header__btn--watchet'),
+  buttonQueue: document.querySelector('.header__btn--queue'),
+}
--- src/js/my-library-page.js
+++ src/js/my-library-page.js
@@ -1,15 +1,16 @@
-// import refs from './refs';
+import refs from './getRefs';
 
-// refs.buttonMyLibrary.addEventListener('click', myLibraryPage);
+refs.buttonMyLibrary.addEventListener('click', myLibraryPage);
 
-// refs.buttonWatched.addEventListener('click', event => {
-//   getLibraryFilms(event, 'watched');
-//   refs.buttonQueue.classList.remove('library-button-current');
-//   refs.buttonWatched.classList.add('library-button-current');
-// });
+refs.buttonWatched.addEventListener('click', (event) => {
+  getLibraryFilms(event, "watched");
+  refs.buttonQueue.classList.remove('library-button-current');
+  refs.buttonWatched.classList.add('library-button-current');
+});
 
-// refs.buttonQueue.addEventListener('click', event => {
-//   getLibraryFilms(event, 'queue');
-//   refs.buttonWatched.classList.remove('library-button-current');
-//   refs.buttonQueue.classList.add('library-button-current');
-// });
+
+refs.buttonQueue.addEventListener('click', (event) => {
+  getLibraryFilms(event, "queue");
+  refs.buttonWatched.classList.remove('library-button-current');
+  refs.buttonQueue.classList.add('library-button-current');
+});
