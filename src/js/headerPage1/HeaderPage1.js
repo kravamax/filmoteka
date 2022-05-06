8c260e8189ad0b1da6870e735c604c702b6c244a
Maksym Kravchenko <maks.cross@gmail.com>
2022-05-06 12:23:24 +0300

    add library page, search dy id

--- src/js/headerPage1/HeaderPage1.js
+++ src/js/headerPage1/HeaderPage1.js
@@ -4,9 +4,9 @@ export default function HeaderPage1() {
         <div class="header-top">
             <div class="logo">
                 <a href='#' class="logo-link">
-                    <!-- <svg class="logo-images" width="24px" height="24px">
-                        <use href="./images/headerPage1/symbol-defs.svg#film"></use>
-                    </svg>-->
+                    <svg class="logo-images" width="24" height="24">
+                        <use href="./symbol-defs.4fba6ab5.svg#film"></use>
+                    </svg>
                     <span class="logo-name">
                         Filmoteka
                 </a>
@@ -31,8 +31,8 @@ export default function HeaderPage1() {
         <form class="header-form">
             <input class='header-input' type="text" name="filmName" placeholder="Поиск фильмов" />
             <button class='header-button--submit' type="submit">
-                <svg width="12px" height="12px" class="input-images">
-                    <use href="./images/headerPage1/symbol-defs.svg#search"></use>
+                <svg width="12" height="12" class="input-images">
+                    <use href="./symbol-defs.4fba6ab5.svg#search"></use>
                 </svg>
             </button>
         </form>
