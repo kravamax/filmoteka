6c5b30f26adf903128833df1ea8be55ae600b459
Maksym Kravchenko <maks.cross@gmail.com>
2022-05-08 15:09:01 +0300

    fix search

--- src/js/search-movies-name.js
+++ src/js/search-movies-name.js
@@ -1,14 +1,17 @@
 import axios from 'axios';
-import createsFilmCardMarkup from "./card-markup";
+// import { render } from 'sass';
+import createsFilmCardMarkup from './card-markup';
 import { buttonHandler } from './modalCard/modalCard';
 import posterSizes from './poster-sizes';
+import { getRefs } from './getRefs';
+const { content } = getRefs();
+// ``;
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/search/movie';
-axios.defaults.headers.get["Content-Type"] = "application/json; charset=utf-8";
+axios.defaults.headers.get['Content-Type'] = 'application/json; charset=utf-8';
 
-const KEY = "067f291d21ed1c6d30bd9ade17d843cc";
+const KEY = '067f291d21ed1c6d30bd9ade17d843cc';
 
-const contentEl = document.getElementById('#content');
 const picturesUrl = `https://image.tmdb.org/t/p/${posterSizes.w342}`;
 let query = '';
 let page = 1;
@@ -17,27 +20,25 @@ async function fetchMovies(query, page) {
   const url = `?api_key=${KEY}&query=${query}&page=${page}`;
   try {
     const response = await axios.get(url);
+
     return response.data;
   } catch (error) {
     console.error(error);
   }
 }
 
-function searchMovies(event) {
+export function searchMovies(event) {
   event.preventDefault();
   page = 1;
   query = event.currentTarget.elements.filmName.value.trim();
-  if (inputValue === '') {
-    return;
-  }
-  contentEl.innerHTML = "";
-  fetchMovies(query).then(renderMarkup);
+  // if (inputValue === '') {
+  //   return;
+  // }
+  content.innerHTML = '';
+  return fetchMovies(query).then(renderMarkup);
 }
 
 function renderMarkup(movies) {
   const searchList = createsFilmCardMarkup(movies, picturesUrl);
-  contentEl.innerHTML('beforeend', searchList);
+  content.insertAdjacentHTML('beforeend', searchList);
 }
-
-
-export { searchMovies };
