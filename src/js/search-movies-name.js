f9c6f8422fef8263ca988689c9b0f4208bed2e57
Yuliia Shybyk <j7557500@gmail.com>
2022-05-08 10:29:51 +0300

    searcer error

--- src/js/search-movies-name.js
+++ src/js/search-movies-name.js
@@ -1,20 +1,17 @@
 import axios from 'axios';
 import createsFilmCardMarkup from "./card-markup";
+import { buttonHandler } from './modalCard/modalCard';
 import posterSizes from './poster-sizes';
-
-export default function searchMovies() {
-  fetchMovies().then(searchMovies).then(renderMarkup);
-}
 
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/search/movie';
 axios.defaults.headers.get["Content-Type"] = "application/json; charset=utf-8";
 
 const KEY = "067f291d21ed1c6d30bd9ade17d843cc";
 
-const formEl = document.querySelector('.header-form');
 const contentEl = document.getElementById('#content');
 const picturesUrl = `https://image.tmdb.org/t/p/${posterSizes.w342}`;
-let inputValue = '';
+let query = '';
+let page = 1;
 
 async function fetchMovies(query, page) {
   const url = `?api_key=${KEY}&query=${query}&page=${page}`;
@@ -26,21 +23,21 @@ async function fetchMovies(query, page) {
   }
 }
 
-formEl.addEventListener('submit', searchMovies);
-
 function searchMovies(event) {
   event.preventDefault();
-
-  inputValue = event.currentTarget.elements.filmName.value.trim();
+  page = 1;
+  query = event.currentTarget.elements.filmName.value.trim();
   if (inputValue === '') {
     return;
   }
-  fetchMovies(inputValue).then(renderMarkup);
   contentEl.innerHTML = "";
-  formEl.reset();
+  fetchMovies(query).then(renderMarkup);
 }
 
 function renderMarkup(movies) {
   const searchList = createsFilmCardMarkup(movies, picturesUrl);
-  contentEl.insertAdjacentHTML('beforeend', searchList);
+  contentEl.innerHTML('beforeend', searchList);
 }
+
+
+export { searchMovies };
