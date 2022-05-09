5c648c2895b23a7c2b8c16eaa5cdb1938ff4048d
Maksym Kravchenko <maks.cross@gmail.com>
2022-05-09 18:04:29 +0300

    firebase

---
+++ src/js/FireBase/firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyA0Jg3owd7CYZ_lII5XL2NnspjHhLrMIPQ',
+  authDomain: 'filmoteka-it-people.firebaseapp.com',
+  projectId: 'filmoteka-it-people',
+  storageBucket: 'filmoteka-it-people.appspot.com',
+  messagingSenderId: '809306812153',
+  appId: '1:809306812153:web:98a43635dcd9e66607fc28',
+};
