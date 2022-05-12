91324ac57ed102450c9a4ac95080631e3ae0635c
Vladyslav Shtompel <vladyslav.shtompel@gmail.com>
2022-05-12 18:32:40 +0300

    Добавил зависимость
    Сделана зависимость библиотеки от пользователя, который вошел на аккаунт

--- src/js/FireBase/onClickStateUSer.js
+++ src/js/FireBase/onClickStateUSer.js
@@ -1,3 +1,4 @@
+
 import { onAuthStateChanged } from 'firebase/auth';
 
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
@@ -19,9 +20,15 @@ import { createNewUser } from '../../index';
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
+//////////////////////////////////////////////////
+const allKeysWatched = [];
+const allKeysQueue = [];
+let currentUser = 0;
+//////////////////////////////////////////////////
+
 let mainKey = 0;
-const keyW = "Key";
-const keyQ = "Key!"
+// const keyW = "Key";
+// const keyQ = "Key!"
 export default function onClickStateUser() {
   onAuthStateChanged(auth, user => {
     if (user) {
@@ -30,6 +37,16 @@ export default function onClickStateUser() {
       const uid = user;
       console.log(uid.email)
       mainKey = user.uid;
+
+          //////////////////////////////////////////
+      currentUser = mainKey;
+      if(!allKeysWatched.includes(mainKey)) {
+        allKeysWatched.push(mainKey);
+      }
+      if(!allKeysQueue.includes(mainKey + "!")) {
+        allKeysQueue.push(mainKey + "!");
+      }
+///////////////////////////////////////////
       putKeyToLocal();
     }
   });
@@ -37,7 +54,19 @@ export default function onClickStateUser() {
 
 function putKeyToLocal() {
   localStorage.setItem('is-Signed-In', true);
-  localStorage.setItem(keyW, mainKey);
-  localStorage.setItem(keyQ, mainKey + "!");
+  // localStorage.setItem(keyW, currentUser);
+  // localStorage.setItem(keyQ, currentUser + "!");
   localStorage.setItem('state-user-Button', true);
+
+    // localStorage.setItem('is-Signed-In', true);
+  // localStorage.setItem(key, currentUser);
+  // localStorage.setItem('state-user-Button', true);
+
+
+  //  /////////////////////////////////////////////////////
+   localStorage.setItem('allKeysWatched', JSON.stringify(allKeysWatched));
+   localStorage.setItem('allKeysQueue', JSON.stringify(allKeysQueue));
+   localStorage.setItem('currentUserWatched', currentUser);
+   localStorage.setItem('currentUserQueue', currentUser + "!")
+   //////////////////////////////////////////////////////
 }
