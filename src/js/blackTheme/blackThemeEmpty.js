28e05983abd08e34183e649cea8ea6b205c68fa6
puerarongo <akhardin96@gmail.com>
2022-05-17 18:38:45 +0300

    final

--- src/js/blackTheme/blackThemeEmpty.js
+++ src/js/blackTheme/blackThemeEmpty.js
@@ -1,6 +1,5 @@
 const blackThemeEmpty = () => {
   if (document.querySelector('.empty__text')) {
-    // console.log(document.querySelector(".empty__text"))
     if (JSON.parse(localStorage.getItem('toggle'))) {
       document.querySelector('.empty__text').classList.add('black--text');
     } else {
